6abcec9f42e16bd1df874929ecda55b2ed7424da
Gabriel-Nog <nogueirajoao799@gmail.com>
2025-01-30 20:13:29 -0300

    v.0.7

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className="not-found-container">
-      <h1>404 - Not Found</h1>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>
+        <Link to="/">Retorne para a página principal</Link>
+      </p>
     </div>
   );
 };
